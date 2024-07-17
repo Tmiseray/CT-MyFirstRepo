c4e1666268115b43ef99cc4f5d71cc59c3012664
Tmiseray <161903165+Tmiseray@users.noreply.github.com>
2024-07-17 16:45:05 -0500

    Completing Exercise 1 in Python!

---
+++ PythonRhythm.py
@@ -0,0 +1,30 @@
+'''Exercise 1
+1. Mulit-line string about favorite hobby/passion
+2. Python statement performing simple calculation'''
+
+# 1. Create Multi-Line String & Print
+MyPassion = '''I am passionate about art! I have been a
+tattoo artist for a few years now. My favorite parts about
+tattooing include:
+1. Letting my creativity flow! Most people come to me with
+an idea of what they may want, but may not have an image.
+I take their ideas and build a few designs for them to
+choose from!
+2. Bringing an idea to life! Once my clients select a
+design, we start on sessions. Each tattoo is different and
+may take shorter or longer time to complete depending on
+detail, size, style, colors, and personal sitting
+tolerance. But once I do finish the tattoo, the expressions
+I see to my art fills my heart and soul with joy! Some
+clients even cry tears of joy! Nothing beats that
+feeling!!'''
+
+print(MyPassion)
+
+# 2. Simple Calculation
+# First I define variables a and b
+a = 6
+b = 7
+
+# Then I print the solution for adding a and b
+print("The sum of a and b is:", (a+b), "!")
